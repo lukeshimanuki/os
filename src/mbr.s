314d3fd5fde30895a9de52a5fb2d6a90f9a58b61
Luke Shimanuki <luke.shimanuki@gmail.com>
2015-04-02 21:59:27 -0700

    small improvements

--- src/mbr.s
+++ src/mbr.s
@@ -7,78 +7,61 @@
 mbr:
 	# set up 4kb stack
 	movw $0x07C0, %ax
-	addw $288, %ax
+	addw $0x0120, %ax
+	cli # disable interrupts
 	movw %ax, %ss
 	movw $4096, %sp
+	sti # enable interrupts
 
+	# set data segment to where we're loaded
 	movw $0x07C0, %ax
 	movw %ax, %ds
 
-	# print
-	mov $0x0E, %ah
-	mov $97, %al # 'a'
-	int $0x10
+	mov %dl, %al # drive number
+	call print
 
-	# load kernel
-
-	# set up registers for int 0x13
-	# get hard drive info
 	mov $0, %bx
 	mov %bx, %es
 	mov %bx, %di
-	mov $0x00, %dl # use 1st floppy (usb)
-	mov $0x08, %ah # int 13 code to get drive parameters
+	mov $8, %ah
 	int $0x13
+	mov %dl, %al # num drives
+	call print
+	mov %dh, %al # num heads - 1
+	call print
+	mov %ch, %al # num cylinders - 1
+	call print
+	mov %cl, %al # num sectors / track
+	call print
 
-	mov %ch, %cl
-	mov $0, %ch
-	mov %cx, %bx # sects/track
+	mov boot_loader, %bx # location of kernel
+	mov %bh, %al # high byte
+	call print
+	mov %bl, %al # low byte
+	call print
 
-	# save num heads
-	add $1, %dh
-	mov %dh, %dl
-	mov $0, %dh
-	push %dx
+	mov $0x09C0, %bx
+	movw $0x5656, (%bx) # set data so we can see if it changed
 
-	# calculate sector
-	mov $1, %ax # start at 2nd sector
-	mov $0, %dx # reset high word for division
-	div %bx # divide sector (ax) by num sectors/track (bx)
-	add $1, %dl # sectors start at 1, not 0
-	mov %dl, %cl # int 0x13 expects sector in cl
-
-	# calculate head
-	mov $1, %ax # start at 2nd sector
-	mov $0, %dx # reset high word for division
-	div %bx # divide sector (ax) by num sectors/track (bx)
-	mov $0, %dx # set remainder to 0
-	pop %bx # num heads
-	div %bx # divide quotient (ax) by num heads (bx)
-	mov %dl, %dh # int 0x13 expects head in dh
-	mov %al, %ch # int 0x13 expects cylinder in ch
+	# load kernel
 
 	# load destination buffer into es:bx
-	mov $0x00, %bx
-	mov %bx, %es # 0x00
-	mov $0x09C0, %bx # 0x07C0 + 512 (boot_loader)
+	mov $0x09C0, %bx
+	mov %bx, %es # 0x0000
+	mov $0x0000, %bx # 0x07C0 + 0x0200 (boot_loader)
 
-	# print stuff
-	mov %ch, %al # cylinder
-	call print
-	mov %dh, %al # head
-	call print
-	mov %cl, %al # sector
-	call print
+	# set parameters
+	mov $0, %ch # cylinder
+	mov $0, %dh # head
+	mov $0, %cl # sector
 
 	# read
-	mov $0x00, %dl # use 1st floppy (usb)
-	mov $2, %ah # read parameter for int 0x13
+	mov $0x80, %dl # use 1st floppy (usb)
+	mov $0x02, %ah # read parameter for int 0x13
 	mov $2, %al # number of sectors (512 bytes each?) to read
 	int $0x13 # read data
 
-	# test print func
-	mov $0x12, %al # "bc"
-	call print
+	call print # actual num read sectors
 
 	# print loaded memory
 	mov $0x09C0, %bx # 0x07C0 + 512 (boot_loader)
